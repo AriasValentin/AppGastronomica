d0074923b717c2b9669e2b06be0be60138780417
Valentin Arias <ariasvalentin1422@gmail.com>
2023-05-28 15:04:59 -0300

    SE HIZO el cambio

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("se hizo un cambio");
+        int variable ;
     }
 }
