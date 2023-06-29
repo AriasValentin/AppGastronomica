51991da9346d49cdb13484ff2735a2c9e0e83bcb
Federico Falotiche <federicofalotiche97@gmail.com>
2023-06-29 18:37:44 -0300

    documentacion para la entrega

---
+++ DOCUMENTACION/JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Model.CE"},{"l":"Model.Clases"},{"l":"Model.Clases.Producto"},{"l":"Model.Clases.Producto.Bebidas"},{"l":"Model.Clases.Producto.Comidas"},{"l":"Model.ExcepcionesPersonalizadas"},{"l":"Model.Interfaces"}];updateSearchResults();
